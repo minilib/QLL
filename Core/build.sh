02f8a1f18b21a809e3813d072503a141344ac1b2
minilib <284627403+minilib@users.noreply.github.com>
2026-07-13 15:16:45 +0900

    refactor: convert build script from .bat to .sh

---
+++ Core/build.sh
@@ -0,0 +1,4 @@
+clang++ -std=c++26 -stdlib=libc++ -g -x c++-module -D_WIN32 -D_CRT_SECURE_NO_WARNINGS "C:/msys64/clang64/share/libc++/v1/std.cppm" --precompile -o "pcm/std.pcm"
+clang++ -std=c++26 -stdlib=libc++ -g -x c++-module -D_WIN32 -D_CRT_SECURE_NO_WARNINGS "include/Base/Macros.cppm" --precompile -o "pcm/Base/Macros.pcm" -fmodule-file=std=../std.pcm
+clang++ -std=c++26 -stdlib=libc++ -g -x c++-module -D_WIN32 -D_CRT_SECURE_NO_WARNINGS  "include/Base/Types.cppm" --precompile -o "pcm/Base/Types.pcm" -fmodule-file=std=../std.pcm
+clang++ -std=c++26 -stdlib=libc++ -g -x c++-module -D_WIN32 -D_CRT_SECURE_NO_WARNINGS  "include/Math/Vector.cppm" --precompile -o "pcm/Base/Vector.pcm"
--- local_tests/asserts.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_ASSERT_H
-#define MY_ASSERT_H
-
-#ifndef GOOGLE_TEST_BRIDGE
-    #define QLL_TEST(TestSuite, TestName) void Dummy_##TestSuite##_##TestName()
-    #define QLL_ASSERT_EQ(actual, expected) (void)(actual); (void)(expected);
-#endif
-
-#endif
