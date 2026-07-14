3fffd4db754c96cecbf4f780da3a716c04f8246d
minilib <284627403+minilib@users.noreply.github.com>
2026-07-15 08:55:20 +0900

    style: convert line endings to LF

--- local_tests/Infra/Log.cpp
+++ local_tests/Infra/Log.cpp
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT-0 OR Zlib OR MIT OR Apache-2.0
 module;
 #include <gtest/gtest.h>
-#include "../asserts.h"
-QLL_TEST() {
-    testing::internal::CaptureStdout();
+import QLL.Core.Base.Types;
+using namespace testing::internal;
+using namespace QLL;
+TEST(loggerTest, OutputsDebug) {
+    CaptureStdout();
+    Log.Log(LogLevel::Debug)
 }
