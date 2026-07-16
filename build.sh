4cdc955c0204cd6c558d3919f20f4e8c2d841a04
minilib <284627403+minilib@users.noreply.github.com>
2026-07-17 08:29:58 +0900

    chore: apply gitignore to current index

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 rm -rf build
 mkdir -p build
-cmake -G Ninja -Wno-experimental -B build -S .
+cmake -G Ninja -Wno-experimental -B build -S . -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
 cp build/compile_commands.json .
 read
