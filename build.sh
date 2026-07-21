971bbba1e5ab0ec8367f2cdd9d4c4028b6cccc21
minilib <284627403+minilib@users.noreply.github.com>
2026-07-21 10:36:05 +0900

    chore: Fixed clangd to work correctly.

--- build.sh
+++ build.sh
@@ -6,5 +6,35 @@ rm -rf build
 mkdir -p build
 cmake -G Ninja -Wno-experimental -B build -S . -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
-cp build/compile_commands.json .
-read
+cp build/Core/CMakeFiles/@cmake_cxx_std.dir/std.pcm pcm
+rm -rf pcm
+mkdir -p pcm
+find build -type f -name "*.pcm" -exec cp -t pcm {} +
+PCMS=()
+for file in pcm/*; do PCMS+=("$(basename "$file" ."${file##*.}")"); done
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    CURRENT_PATH=$(pwd -W)
+else
+    CURRENT_PATH=$PWD
+fi
+MODULE_FILES=""
+for name in "${PCMS[@]}"; do MODULE_FILES=$(cat << EOF
+$MODULE_FILES
+-fmodule-file=$name=$CURRENT_PATH/pcm/$name.pcm,
+EOF
+); done
+cat << EOF > .clangd
+CompileFlags:
+    Add: [
+        "-std=c++26",
+        "-stdlib=libc++",
+        "-g",
+        "-D_WIN32",
+        "-D_CRT_SECURE_NO_WARNINGS",
+        "-IC:/dev/C++/QLL",
+        "-IC:/dev/C++/QLL/Core/include",$MODULE_FILES
+    ]
+Index:
+  Background: Build
+EOF
+cat .clangd
